f7cff6252dcae3cc88f5c16057c8c7793fe5e67c
amirkhan1092 <amirkhan1092@gmail.com>
2020-01-20 23:56:10 +0530

    words reverse in a string

--- words_order_string.py
+++ words_order_string.py
@@ -10,6 +10,22 @@ k = ' '.join(l)
 
 print(f'original string:> {g}')
 print(f'reverse string:> {k}')
+
 # output
 # original string:> this is python class
 # reverse string:> class python is this
+
+
+# reverse words character
+st = input('add paragraph ')
+
+ls = st.split()  # list of words
+p = []
+for k in ls:
+    p.append(k[::-1])
+out = ' '.join(p)
+print(f'original string:> {st}')
+print(f'reverse string:> {out}')
+
+
+
