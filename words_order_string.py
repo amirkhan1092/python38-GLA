f0014ebe01c6ffb8029f387705b5211bff7bc7e9
amirkhan1092 <amirkhan1092@gmail.com>
2020-01-22 10:08:49 +0530

    maximum length of word 1st apply

--- words_order_string.py
+++ words_order_string.py
@@ -1,34 +1,46 @@
-# order of words in a string
-# reverse
-g = input('enter the string ')
+# # order of words in a string
+# # reverse
+# g = input('enter the string ')
+#
+# l = g.split()  # split with whitespace separator may '\n',' ','\t' etc
+#
+# l.reverse()
+#
+# k = ' '.join(l)
+#
+# print(f'original string:> {g}')
+# print(f'reverse string:> {k}')
+#
+# # output
+# # original string:> this is python class
+# # reverse string:> class python is this
+#
+#
+# # reverse words character
+# st = input('add paragraph ')
+#
+# ls = st.split()  # list of words
+# # p = []
+# c = 0
+# for k in ls:
+#     # p.append(k[::-1])
+#     ls[c] =  k[::-1]
+#     c += 1
+# out = ' '.join(ls)
+# print(f'original string:> {st}')
+# print(f'reverse string:> {out}')
+#
 
-l = g.split()  # split with whitespace separator may '\n',' ','\t' etc
-
-l.reverse()
-
-k = ' '.join(l)
-
-print(f'original string:> {g}')
-print(f'reverse string:> {k}')
-
-# output
-# original string:> this is python class
-# reverse string:> class python is this
+h = input('enter the string')
+ls = h.split()
+word_max = max(ls,key=len)
+word_min = min(ls,key=len)
+print(word_max)
+print(word_min)
 
 
-# reverse words character
-st = input('add paragraph ')
 
-ls = st.split()  # list of words
-# p = []
-c = 0
-for k in ls:
-    # p.append(k[::-1])
-    ls[c] =  k[::-1]
-    c += 1
-out = ' '.join(ls)
-print(f'original string:> {st}')
-print(f'reverse string:> {out}')
+
 
 
 
