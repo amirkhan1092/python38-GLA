9df5619a4d1b03a1b14674e9c7263fa39fa0fc18
amirkhan1092 <amirkhan1092@gmail.com>
2020-01-24 09:50:19 +0530

    list init, assignment, typecast, mutable, slicing, indexing

--- list_example.py
+++ list_example.py
@@ -11,3 +11,21 @@ print(st)
 print(ls)
 # output hello
 # output ['h', 'e', 'l', 'l', 'o']
+
+# indexing
+# get the element at given position
+ls = [1, 3, 65, 76, 'hello']
+d = ls[0]
+print(d)
+# output 1
+# slicing
+# get the subsequence(sub list ) at given range
+ls = [1, 3, 65, 76, 'hello']
+d = ls[-1:40]
+print(d)
+# output [1, 3, 65, 76]
+# changeable
+ls = [2, 5, 4, 43]
+ls[0] = 'hello'
+print(ls)
+
