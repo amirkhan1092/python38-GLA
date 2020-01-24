50e8bbdb8ba233c3d15add5fffbbdc6c7078639c
amirkhan1092 <amirkhan1092@gmail.com>
2020-01-24 09:15:30 +0530

    list init, asign, typecast

---
+++ list_example.py
@@ -0,0 +1,13 @@
+# initialize the list (empty list)
+ls = []
+ls = list()
+# output []
+# init a list with single integer value
+ls = [10]
+#  output [10]
+st = '121221212122221212122'
+ls = list(st)
+print(st)
+print(ls)
+# output hello
+# output ['h', 'e', 'l', 'l', 'o']
