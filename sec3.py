e09bea14ad417fe9ab1a70bc701374fa7ac3004e
amirkhan1092 <amirkhan1092@gmail.com>
2020-02-04 09:09:36 +0530

    string methods

--- sec3.py
+++ sec3.py
@@ -12,3 +12,42 @@ print(d, type(d))
 h = 'hello world'
 d = h[-3::1]
 print(d, type(d))
+
+
+# type cast with string
+# number
+k = int('1010',8)
+print(k, type(k))
+
+k = float('2.4')
+print(k, type(k))
+
+
+# list
+st = '[2, 43, 4]'
+lst = list(st)
+print(lst, type(lst))
+
+lst2 = eval(st)
+print(lst2, type(lst2))
+
+hello = 10
+
+k = eval('hello')
+print(k, type(k))
+
+# list from from user
+lst = eval(input('enter the list '))
+print(lst)
+
+
+# method
+
+# split
+k = '213 43 443 23 23  212'
+out = k.split()
+
+print(out, type(out))
+
+
+
