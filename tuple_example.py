a526521a256fcee185ffec68c68ed5e2f35423ea
amirkhan1092 <amirkhan1092@gmail.com>
2020-02-07 09:57:58 +0530

    colon tuple

--- tuple_example.py
+++ tuple_example.py
@@ -125,7 +125,31 @@ print(out)  # 1
 out = t.count('Hello')
 print(out)  # 0
 
+# colon
+import copy
+h = (2, 3, 8, [])
+k = copy.deepcopy(h)
+h[-1].append(100)
+print(h[-1], id(h[-1]))
+print(k[-1], id(k[-1]))
 
 
+# deepcopy using raw data
+
+k = [2, 4, [3, 5]]
+raw_data = str(k)
+print(raw_data, type(raw_data))
+
+lst = eval(raw_data)
+
+
+
+lst =  [(10, 20, 40), (40, 50, 60), (70, 80, 90)]
+for i in range(len(lst)):
+    tt = list(lst[i])
+    tt[-1] = 100
+    tt = tuple(tt)
+    lst[i] = tt
+print(lst)
 
 
