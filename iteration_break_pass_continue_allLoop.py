73e4c5194b6929c8dd18924172fc0a8dc64b6001
amirkhan1092 <amirkhan1092@gmail.com>
2020-02-14 00:41:52 +0530

    python loop

---
+++ iteration_break_pass_continue_allLoop.py
@@ -0,0 +1,27 @@
+
+# Python Loop
+1
+# 1. for loop
+'''
+The for statement in Python differs a bit from what you may be used to in C or Pascal. 
+Rather than always iterating over an arithmetic progression of numbers, 
+or giving the user the ability to define both the iteration step and halting condition (as C), 
+
+Python’s for statement iterates over the items of any sequence 
+(a list or a string), in the order that they appear in the sequence. 
+For example :
+
+'''
+
+vs = 'hello python'
+for i in vs:
+    print(i)
+
+
+
+
+
+
+
+
+
