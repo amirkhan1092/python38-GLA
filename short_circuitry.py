542e7e0cfb10fde3fc228bffbef6f16863f4f381
amirkhan1092 <amirkhan1092@gmail.com>
2020-02-16 01:25:57 +0530

    Kadane’s Algorithm: maximum contiguous sum of list

--- short_circuitry.py
+++ short_circuitry.py
@@ -4,6 +4,46 @@ False Evaluate Quantity
 0, None, '', [], (), {}, set(), False
 '''
 
+
+
+
+
+
+
+
+
+
+
+# combine two list
+
+l1 = [2, 6, 8, 2, 9, 76]
+l2 = [4, 8, 1, 0]
+# l = [(l1[i], l2[i]) for i in range(len(l1))]
+cc = len(l1) if len(l1) < len(l2) else len(l2)
+l = []
+for i in range(cc):
+    l.append((l1[i], l2[i]))
+
+print(l)
+print(l1)
+print(l2)
+
+
+
+
+
+
+lst = [(2, 4), (6, 8), (8, 1), (2, 0)]
+l1 = []
+l2 = []
+for i,k in lst:
+    l1.append(i)
+    l2.append(k)
+print(l1)
+print(l2)
+1,2,3,4,
+
+
 a = 10
 b = 30
 
