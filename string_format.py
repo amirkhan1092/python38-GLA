bc7c80f85e934958f819e564a5e10a6ec1a2e43c
amirkhan1092 <amirkhan1092@gmail.com>
2020-02-17 14:15:22 +0530

    string format

--- string_format.py
+++ string_format.py
@@ -5,10 +5,22 @@ This method lets us concatenate elements within a string through positional form
 '''
 
 
-
 a = 10
 b = 30
 c = a + b
+temp = "result is {} of {} and {}"
+f = temp.format(c, a, b)
+print(f)  # result is 40 of 10 and 30
+
+# tag f format
+a = 10
+b = 30
+c = a + b
+temp = f"result is {c} of {a} and {b} 😙"
+
+print(temp)  # result is 40 of 10 and 30
+
+
 f = f'result is {c} of {a} {b}'
 print(f)  # result is 40 of 10 30
 
@@ -23,9 +35,6 @@ print(f)  # result is 40 of 10 30
 
 f = 'result is %d of %d %d' % (c, a, b)
 print(f)
-
-
-
 
 
 
