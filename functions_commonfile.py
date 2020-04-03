3e16e5fa6e15391bdb52610a12907b94e8b569a5
Amir khan <amirkhan1092@gmail.com>
2020-04-03 12:11:39 +0530

    functions in python

--- functions_commonfile.py
+++ functions_commonfile.py
@@ -1,6 +1,34 @@
 # function
-def Add(a:int,b:int):
-    return a+b
+def Sum(n1, n2):
+    return n1 + n2
 
-res = Add(2, 6)
+num1 = 12
+num2 = 21
+res = Sum(num1, num2)
 print(res)
+
+
+def print_sum(n1, n2):
+    print('Sum of two numbers is ', n1 + n2)
+
+num1 = 12
+num2 = 21
+print_sum(num1, num2)
+
+def type_sum(v1, v2):
+    type1 = [int, list, str, tuple]
+    type2 = [dict, set]
+    if type(v1) in type1 and type(v2) in type1:
+        res = value1 + value2
+        return f'value is ordered or integer number, sum is {res}'
+    else:
+        value1.update(value2)
+        return f'values are unordered merge file {value1}'
+
+value1 = eval(input('enter the first value '))
+value2 = eval(input('enter the second value '))
+res = type_sum(value1, value2)
+print(res)
+
+
+
