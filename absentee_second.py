7d8abe59d49bd1efd479aa8040cfe2c9356a1d91
dbc2201 <divyanshb22@gmail.com>
2020-04-16 02:51:28 +0530

    PEP commit

--- absentee_second.py
+++ absentee_second.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import re
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 
@@ -8,34 +7,37 @@ root.configure(background='light green')
 root.title('GLA Online attedance absentee(based on official Email ID)')
 root.geometry("500x250")
 
+
 def browsefunc():
-    root.mainfile = 'sec'+section.get()+'.csv'  # local csv file having all the details of the students with official emails 
-    
-    root.filename = askopenfilename() # browse csv file for from zoom 
-def calc():    
+    root.mainfile = 'sec' + section.get() + '.csv'  # local csv file having all the details of the students with official emails
+
+    root.filename = askopenfilename()  # browse csv file for from zoom
+
+
+def calc():
     data = pd.read_csv(root.filename)
     data_main = pd.read_csv(root.mainfile)
     useremail = set(data['User Email'])
     main1 = data_main['Official Email-ID']
     main2 = data_main['Class Roll No']
     main_dict = dict(zip(main1, main2))
-    
-    data = list(set(main1)-set(useremail))
+
+    data = list(set(main1) - set(useremail))
     ls = []
     for i in data:
         ls.append(main_dict.get(i))
-    ls.sort()    
+    ls.sort()
     pathlabel.config(text=str(ls))
+
 
 ##lastrolln = IntVar()
 ##lastrolln.set(70)
 section = StringVar()
 section.set('A')
 
-
 ll1 = Label(root, text="Enter the section")
 ll1.pack()
-entry2 = Entry(root, textvariable=section )
+entry2 = Entry(root, textvariable=section)
 entry2.pack()
 ##entry.grid(columnspan=4, ipadx=70) 
 
